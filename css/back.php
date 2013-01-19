a829031bafb7714d2ca66b8ecc43d98cf268751a
Dušan Vala <rellik@email.cz>
2013-01-19 14:46:00 +0100

    Update PDF wiever, SEO optimalize
    Signed-off-by: Dušan Vala <rellik@email.cz>

--- css/back.php
+++ css/back.php
@@ -45,11 +45,13 @@ if (empty($color)) {
 } else {
     $soubor = '../logo/logo_' . $color . '.png';
 }
+
 if (!$logo) {
     $logo = '../logo/logo_mmrs.png';
 } else {
     $logo = '../logo/logo_' . $logo . '.png';
 }
+
 if ($backgroundRepeat == 'r') {
     $repeat = 'right';
 }
@@ -81,7 +83,6 @@ body {
     border-bottom: 1px solid <?php echo '#'.$ContentColor; ?>;
 }
 #header div.title {
-    background-repeat: <?php echo $repeatX; ?>;
     margin-left: <?php echo $margin; ?>px;
     color: #<?php echo $title[0]; ?>;
     text-shadow: #<?php echo $title[1]; ?> 1px 1px 2px;
