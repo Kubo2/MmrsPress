fce04a011474ee2fcc559ebc5393ea731d64f7a4
Dušan Vala <rellik@email.cz>
2013-01-20 10:43:19 +0100

    Update PDF wiever, SEO optimalize
    Signed-off-by: Dušan Vala <rellik@email.cz>

--- app/models/model.php
+++ app/models/model.php
@@ -98,7 +98,6 @@ class Model extends Nette\Object {
 
                     CREATE TABLE IF NOT EXISTS `mmrs_photos` (
                       `id` int(11) NOT NULL AUTO_INCREMENT,
-                      `name` varchar(35) COLLATE utf8_bin NOT NULL,
                       `label` varchar(300) COLLATE utf8_bin NOT NULL,
                       `photo` varchar(30) COLLATE utf8_bin NOT NULL,
                       `folder` varchar(35) COLLATE utf8_bin NOT NULL,
