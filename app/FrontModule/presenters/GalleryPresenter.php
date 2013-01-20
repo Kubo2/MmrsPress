a3f0dd034024b195a28524052aa74555f4efb0e3
Dušan Vala <rellik@email.cz>
2013-01-20 19:43:56 +0100

    Update PDF wiever, SEO optimalize
    Signed-off-by: Dušan Vala <rellik@email.cz>

--- app/FrontModule/presenters/GalleryPresenter.php
+++ app/FrontModule/presenters/GalleryPresenter.php
@@ -8,8 +8,6 @@ namespace FrontModule;
  * http://mmrspress.eu
  * 
  */
-use Nette\Application\UI\Form;
-use Nette\Utils\Paginator;
 use Nette\Application\UI\Control;
 use Nette\Http\User;
 
@@ -18,15 +16,17 @@ class GalleryPresenter extends \BasePresenter {
     public function beforeRender() {
         parent::beforeRender();
         // výpis posledních 4 přidaných fotek z veřejných galerií
-        $public = $this->model->getGallery()->order('id ASC')->where('public', '1');
-        $wiews_last = array();
-        foreach ($public as $wiew) {
-            $wiews_last[] = $wiew->folder;
+        $lastImage = $this->model->getPhotos()->order('id DESC')->limit(4);
+        $result = array();
+        foreach ($lastImage as $wiew) {
+            foreach ($this->model->getGallery()->where('folder', $wiew->folder) as $name) {
+                $result[] = array($wiew->folder => array($wiew->photo => $name->name));
+            }
         }
-        $this->template->photo_last = $this->model->getPhotos()->where('folder', $wiews_last)->order('id DESC')->limit(4);
+        $this->template->wiews_last = $result;
+
         // výpis veřejných fotogalerií
-        $this->template->galerie = $this->model->getGallery()
-                        ->order('id DESC')->where('public', '1');
+        $this->template->galerie = $this->model->getGallery()->order('id DESC')->where('public', '1');
         require_once LIBS_DIR . '/galleryFunction.php'; // funkce pro náhodný obrázek (libs)
 
         $panel = $this->model->getSettings()->select("*")->where('select', 'photoPanel')->fetch();
@@ -36,4 +36,5 @@ class GalleryPresenter extends \BasePresenter {
             $this->template->photoPanel = $panel->public;
         }
     }
+
 }
